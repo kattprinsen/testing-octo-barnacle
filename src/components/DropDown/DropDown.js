561cf13508cc753e31a7418991488691b2edc255
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-11-26 11:44:22 +0100

    Fixed the dropDown

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -14,15 +14,15 @@ const DropDown = ({ title, header, items }) => {
 
   return (
     <div className="dropdown-container">
-      <span className="dropdown-title" data-testid="title-id" value={title}> {title} </span>
-      <div className="dropdown-header" onClick={toggling} data-testid="header-id" value={header}>
+      <span className="dropdown-title" data-testid="title-id"> {title} </span>
+      <div className="dropdown-header" onClick={toggling} data-testid="header-id">
       {selectedOption || `${header}`}
       </div>
       {isOpen && (
         <div className="dropdown-list-container">
           <ul className="dropdown-list">
-          {items && items.map((index, item) => (
-            <li className="dropdown-list-item" data-testid="item-id" onClick={onOptionClick(item)} key={Math.random()} value={item}>
+          {items && items.map(item => (
+            <li className="dropdown-list-item" data-testid="item-id" onClick={onOptionClick(item)} key={Math.random()}>
               {item}
             </li>
           ))}
