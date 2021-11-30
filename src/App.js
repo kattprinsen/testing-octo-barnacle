80baa5078dd93d043be68eec067c250b3e52138a
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-11-30 09:43:38 +0100

    Working working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import DropDown from './components/DropDown/DropDown';
 import Button from './components/Button/Button';
 
+const countryData = {
+  title: 'Country',
+  header: 'Select a country',
+  items: ['Sweden', 'Norge', 'Danmark']
+}
+
+const nodeData = {
+  title: 'Node',
+  header: 'Select a node',
+  items: ['Malmö', 'Stockholm', 'Köpenhamn']
+}
+
+const flowData = {
+  title: 'Flow',
+  header: 'Select a flow',
+  items: ['Order pickup', 'Delivery', 'In-store']
+}
+
 
 function App() {
-
-  const countryData = {
-    title: 'Country',
-    header: 'Select a country',
-    items: ['Sweden', 'Norge', 'Danmark']
-  }
-
-  const nodeData = {
-    title: 'Node',
-    header: 'Select a node',
-    items: ['Malmö', 'Stockholm', 'Köpenhamn']
-  }
-
-  const flowData = {
-    title: 'Flow',
-    header: 'Select a flow',
-    items: ['Order pickup', 'Delivery', 'In-store']
-  }
 
   console.log(nodeData);
 
