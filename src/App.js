fe25c694f9bd8ac2de0f8298ac5358ebcad07e38
Kattprinsen <1647312+kattprinsen@users.noreply.github.com>
2021-12-08 11:17:47 +0100

    Missed in last commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import DropDown from './components/DropDown/DropDown';
 import Button from './components/Button/Button';
 
